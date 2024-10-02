57771691cbbd175137e9dd60cf6e51a31208bd81
nhan295 <tranthihongnhan295@gmail.com>
2024-10-02 16:34:28 +0700

    create login/signup.vue file

---
+++ frontend/src/view/login.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div>
+        <div>
+            <h3>Login</h3>
+            <hr />
+        </div>
+        <form>
+            <div class="form-group">
+                <label>User name</label>
+                <input type="text" class='form-control' name="full_name" placeholder="Enter User name" required>
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class='form-control' name="password" placeholder="Enter password" required>
+            </div>
+
+            <div class="btn-sub">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+  
+
+
+
+
+<script>
+export default{};
+</script>
---
+++ frontend/src/view/signup.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+     <div>
+         <h3>Signup</h3>
+         <hr />
+     </div>
+     <form>
+         <div class="form-group">
+             <label>User name</label>
+             <input type="text" name="full_name" placeholder="Enter User name" required>
+         </div>
+
+         <div class="form-group">
+             <label>Password</label>
+             <input type="password"  name="password" placeholder="Enter password" required>
+         </div>
+
+         <div class="form-group">
+             <label>Email</label>
+             <input type="password"  name="password" placeholder="Enter email" required>
+         </div>
+
+         <div class="form-group">
+             <label>Phone number</label>
+             <input type="password"  name="password" placeholder="Enter phone number" required>
+         </div>
+
+         <div class="form-group">
+             <label>Address</label>
+             <input type="password"  name="password" placeholder="Enter address" required>
+         </div>
+
+         <div class="btn-sub">
+             <button type="submit">Signup</button>
+         </div>
+
+     </form>
+ </div>
+</template>
+ 
+<script>
+export default{};
+</script>
