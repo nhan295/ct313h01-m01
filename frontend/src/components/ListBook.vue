d13f12ff25025c3d1e7e886d66d63a4dc418e830
nhan295 <tranthihongnhan295@gmail.com>
2024-10-05 22:31:31 +0700

    create new file

---
+++ frontend/src/components/ListBook.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="list">
+        <div class="list-item">
+
+            <img src="D:\ct313h01-m01\frontend\src\assets\domxanhmo.jpg" alt="">
+            <span>TEN SEXH</span>
+            <span>gia: 12345D</span>
+        </div>
+
+        <div class="list-item">
+
+            <img src="D:\ct313h01-m01\frontend\src\assets\domxanhmo.jpg" alt="">
+            <span>TEN SEXH</span>
+            <span>gia: 12345D</span>
+        </div>
+
+        <div class="list-item">
+
+            <img src="D:\ct313h01-m01\frontend\src\assets\domxanhmo.jpg" alt="">
+            <span>TEN SEXH</span>
+            <span>gia: 12345D</span>
+        </div>
+
+        <div class="list-item">
+
+            <img src="D:\ct313h01-m01\frontend\src\assets\domxanhmo.jpg" alt="">
+            <span>TEN SEXH</span>
+            <span>gia: 12345D</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+    .list {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .list-item {
+        display: flex;
+        height: 100%;
+        /* margin: auto 0; */
+        align-items: center;
+        flex-direction: column;
+        border: 1px solid blue;
+        border-radius: 4px;
+    }
+    .list-item img {
+        width: 50%;
+    }
+</style>
